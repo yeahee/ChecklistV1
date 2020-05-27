15ce9f223f738e4e5384c4903777917e3261a2da
yeahee <dpgml40234023@gmail.com>
2020-05-27 14:41:20 +0900

    make the checklist checkable

--- ChecklistV1/ContentView.swift
+++ ChecklistV1/ContentView.swift
@@ -8,13 +8,38 @@
 
 import SwiftUI
 
+struct ChecklistItem: Identifiable{
+    let id = UUID()
+    var name: String
+    var isChecked: Bool = false
+}
+
 struct ContentView: View {
-    @State var checklistItems = ["Walk the dog", "Brush my teeth", "Learn iOS development", "Soccer practice", "Eat ice cream"]
+    // Properties
+    // ==========
+    @State var checklistItems = [
+        ChecklistItem(name: "Walk the dog", isChecked: false),
+        ChecklistItem(name: "Brush my teeth", isChecked: false),
+        ChecklistItem(name: "Learn iOS development", isChecked: true),
+        ChecklistItem(name: "Soccer practice", isChecked: false),
+        ChecklistItem(name: "Eat ice cream", isChecked: true)
+    ]
     var body: some View {
         NavigationView{
             List{
-                ForEach(checklistItems, id: \.self){
-                    item in Text(item)
+                ForEach(checklistItems){
+                    checklistItem in
+                    HStack{
+                        Text(checklistItem.name)
+                        Spacer()
+                        Text(checklistItem.isChecked ? "☑️" : "⬛️")
+                    }// End of HStack
+                    .background(Color.white)
+                    .onTapGesture {
+                        if let matchingIndex = self.checklistItems.firstIndex(where: {$0.id == checklistItem.id}){
+                            self.checklistItems[matchingIndex].isChecked.toggle()}
+                        self.printChecklistContents()
+                    }
                 }// End of ForEach
                 .onDelete(perform: deleteListItem)
                 .onMove(perform: moveListItem)
