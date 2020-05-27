4731f5e1d269b27918bc08ae3368a026dd981f4f
yeahee <dpgml40234023@gmail.com>
2020-05-27 13:46:51 +0900

    change the item of list by .onTapGesture

--- ChecklistV1/ContentView.swift
+++ ChecklistV1/ContentView.swift
@@ -9,40 +9,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var checklistItems = ["Walk the dog", "Brush my teeth", "Learn iOS development", "Soccer practice", "Eat ice cream"]
     var body: some View {
         NavigationView{
             List{
-                Section(header: Text("High priority")){
-                    Group{
-                        Text("Walk the dog")
-                        Text("Brush my teeth")
-                        Text("Learn iOS development")
-                        Text("Make dinner")
-                        Text("Do laundry")
-                        Text("Pay bills")
-                    }
-                    Group{
-                        Text("Finish homework")
-                        Text("Change internet provider")
-                        Text("Clean the kitchen")
-                        Text("Wash the car")
-                        Text("Wash the hair")
-                    }
+                Text(checklistItems[0]).onTapGesture {
+                    self.checklistItems[0] = "Take the dog to the vet"
                 }
-                Section(header: Text("Low priority")){
-                    Text("Soccer practice")
-                    Text("Eat ice cream")
-                    Text("Take vocal lessons")
-                    Text("Record hit single")
-                    Text("Learn every martial art")
-                    Text("Design costume")
-                    Text("Design crime-fighting vehicle")
-                    Text("Come up with superhero name")
-                    Text("Befriend space raccoon")
-                    Text("Save the world")
-                }
+                Text(checklistItems[1])
+                Text(checklistItems[2])
+                Text(checklistItems[3])
+                Text(checklistItems[4])
             }// End of List
-            .listStyle(GroupedListStyle())
             .navigationBarTitle("Checklist")
         }// End of Navigation View
     }// End of body
